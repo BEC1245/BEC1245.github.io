c9b1ffa678e2d0fbe3e86e6e6c1c6523e21dd707
chan <chanymini1@gmail.com>
2024-06-11 16:06:58 +0900

    note finish

--- page/js/note.js
+++ page/js/note.js
@@ -1,3 +1,6 @@
+// 초기 로딩시 
+
+
 function getSelected() { // 현제 선택된 아이탬
     return localStorage.getItem('selected')
 }
@@ -24,38 +27,25 @@ function findIdx(str, count) { // 위치
 }
 
 function writeNode() { // node 초기화시 발동
-    const { inputContents } = elements()
     let selected = getSelected()
     let length = 0;
-    const date = new Date()
 
-    for(n = 0; n < inputContents.length; n++) {
-        if(localStorage.getItem(n) !== null) {
-            length++
+    while (true) {
+        if(localStorage.getItem(length) === null) {
+            break;
         }
-    }
-
-    console.log(length);
-    if(length === 0) {
-        let setString = '';
-        let idx = 0;
-        for(const b of inputContents) {
-            setString += `${b.className}${idx++}:[] `
-        }
-
-        localStorage.setItem('0', `title:[공백] writer:[공백] date:[${date.toLocaleDateString().replace(/ /g,"")}] ${setString}`)
-        length = 1;
+        length++
     }
 
     if(localStorage.getItem(selected) == null) {
         localStorage.setItem('selected', '0')
     }
 
-    const obj = document.querySelector('.ui')
+    const obj = document.querySelector('.ui-container')
     obj.innerHTML = '';
 
-    // 절때 title: 위치 바꾸지 말 것 오류남
-    for(n = 0; n < length; ++n){
+    console.log(length);
+    for(n = 0; n < length; ++n){ // node 불러오기
         const contents = localStorage.getItem(n);
 
         console.log(contents);
@@ -122,6 +112,7 @@ function save() {
 
     localStorage.setItem(selected, setString)
     load(selected)
+    writeNode()
 }
 
 function reset() {
@@ -131,7 +122,11 @@ function reset() {
 
     console.log(ele.inputIds);
     for(const a of ele.inputIds) {
-        setString += `${a.id}:[${a.value}] `
+        if(a.id === 'date') {
+            setString += `${a.id}:[${a.value}] `
+            continue
+        }
+        setString += `${a.id}:[공백] `
     }
 
     let idx = 0
@@ -140,16 +135,24 @@ function reset() {
     }
 
     localStorage.setItem(selected, setString) // 아이탬 삭제시 이건 다른걸로 옴겨야 한다.
+    
     load(selected)
+    writeNode()
 }
 
 function nodeAdd() {
     let length = 0;
+
     while (true) {
         if(localStorage.getItem(length) === null) {
             break;
         }
         length++
+    }
+
+    if(length === 0) { 
+        const note = document.querySelector('.note-invi')
+        note.className = 'note'
     }
 
     const { inputContents } = elements()
@@ -164,4 +167,53 @@ function nodeAdd() {
     writeNode()
     load(length)
 }
+
+function nodeRemove(target) { 
+    let length = 0;
+
+    while (true) {
+        if(localStorage.getItem(length) === null) {
+            break;
+        }
+        length++
+    }
+
+    for(n = target + 1; n < length; n++) {
+        const info = localStorage.getItem(n);
+        localStorage.setItem(n - 1, info)
+    }
+    localStorage.removeItem(length - 1)
+
+    // 노드가 없는 경우 처리
+    if(length - 1 === 0) { 
+        const note = document.querySelector('.note')
+        note.className = 'note-invi'
+        writeNode()
+        return
+    }
+    writeNode()
+    load(0)
+}
+
+function initalLoad() {
+    let length = 0;
+
+    while (true) {
+        if(localStorage.getItem(length) === null) {
+            break;
+        }
+        length++
+    }
+
+    const note = document.querySelector('.note')
+    if(length === 0) { 
+        note.className = 'note-invi'
+    } else {
+        note.className = 'note'
+        load(0)
+    }
+    writeNode()
+}
+
+initalLoad()
 
